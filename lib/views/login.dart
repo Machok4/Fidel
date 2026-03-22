ef4d130d048fcb8f9fade590034a9e79dfa43847
Machok4 <m4ch0k4@gmail.com>
2026-03-22 21:17:03 +0300

    Update login.dart

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -12,7 +12,7 @@ TextEditingController passwordController = TextEditingController();
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
-
+ 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
